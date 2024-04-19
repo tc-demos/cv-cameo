6d813709dbb9152a1dd6bdbf0dc6f7f6817b2cfb
Joey <69287308+Joey0980@users.noreply.github.com>
2024-04-19 14:09:18 -0400

    add filters

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+
+import filters
 from managers import WindowManager, CaptureManager
 
 
@@ -7,14 +9,37 @@ class Cameo(object):
         self._windowManager = WindowManager('Cameo', self.onKeyPress)
         self._captureManager = CaptureManager(cv2.VideoCapture(0), self._windowManager, True)
 
-    def run(self):
+        self._LFilters = [{"title": "No filtering", "filter": None}]
+
+        self._sharpenFilter = filters.SharpenFilter()
+        self._LFilters.append({"title": "Sharpen", "filter": self._sharpenFilter})
+
+        self._findEdgesFilter = filters.FindEdgesFilter()
+        self._LFilters.append({"title": "Find edges", "filter": self._findEdgesFilter})
+
+        self._blurFilter = filters.BlurFilter()
+        self._LFilters.append({"title": "Blur", "filter": self._blurFilter})
+
+        self._embossFilter = filters.EmbossFilter()
+        self._LFilters.append({"title": "Emboss", "filter": self._embossFilter})
+
+
+
+    def run(self, choice):
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
             frame = self._captureManager.frame
             if frame is not None:
+                if choice == 0:
+                    pass
+                elif 0 < choice <= len(self._LFilters):
+                    self._LFilters[choice]["filter"].apply(frame, frame)
+                elif choice == len(self._LFilters):
+                    filters.enhanceEdges(frame, frame)
+                else:
+                    pass
 
-                pass
             self._captureManager.exitFrame()
             self._windowManager.processEvents()
 
@@ -31,8 +56,29 @@ class Cameo(object):
 
 
 def main():
-    print("Space: screenshot\nTab: start-stop screencast\nEscape: exit")
-    Cameo().run()
+    cam = Cameo()
+    while True:
+        print("Space: screenshot\nTab: start-stop screencast\nEscape: exit")
+
+        for i, f in enumerate(cam._LFilters):
+            print(f"{i}: {f['title']}")
+
+        idx = input(len(cam._LFilters))
+        print(idx, "Enhance edges")
+
+        print()
+
+        choice = -2
+        while choice < -1 or choice > len(cam._LFilters):
+            choice = int(input("Choose a filter: "))
+        print()
+
+        if choice > -1:
+            cam.run(choice)
+            ans = input("Another? (y/n): ")
+        else:
+            break
+
 
 
 if __name__ == '__main__':
